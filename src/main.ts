5033a3fedb3120584f54cf744ca4989c7f11a546
ImanBurluk <jaroslavzev63@mail.ru>
2024-05-26 20:57:45 +0500

    Make scene Durotar

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import Phaser from 'phaser';
 import './style.css';
+import { Durotar } from './scenes/durotar';
 
 new Phaser.Game({
 	width: 800,
 	height: 600,
 	title: 'Phaser RPG',
+	scene: Durotar,
 	url: import.meta.env.URL || '',
 	version: import.meta.env.VERSION || '0.0.1',
 	backgroundColor: '#000',
